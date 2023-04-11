3ee0e517af5fc1e35ac04498e133e819de0badc0
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2023-04-11 16:34:52 -0300

    Continuando o projeto Canil

--- Mod02/Canil/src/controllers/pageController.ts
+++ Mod02/Canil/src/controllers/pageController.ts
@@ -1,18 +1,43 @@
 import { Request, Response} from 'express';
+import { createMenuObject} from '../helpers/createMenuObject'
 
 export const home = (req: Request, res:Response) => {
-    res.send('home')
-    //res.render('pages/page')
+    //res.send('home')
+    res.render('pages/page',{
+        menu:createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background:'allanimals.jpg'
+        }
+    })
 }
 export const dogs = (req: Request, res:Response) => {
-    res.send('Cães')
-    //res.render('pages/page')
+    //res.send('Cães')
+    res.render('pages/page',{
+        menu:createMenuObject('dog'),
+        banner: {
+            title: 'Cães',
+            background:'banner_dog.jpg'
+        }
+    })
 }
 export const cats = (req: Request, res:Response) => {
-    res.send('Gatos')
-    //res.render('pages/page')
+    //res.send('Gatos')
+    res.render('pages/page',{
+        menu:createMenuObject('cat'),
+        banner:{
+            title: 'Gatos',
+            background:'banner_cat.jpg'
+        }
+    })
 }
 export const fishes = (req: Request, res:Response) => {
-    res.send('Peixes')
-    //res.render('pages/page')
+    //res.send('Peixes')
+    res.render('pages/page',{
+        menu:createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background:'banner_fish.jpg'
+        }
+    })
 }
